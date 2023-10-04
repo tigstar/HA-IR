1e270329cf7bb684cbd9896af38f0ad18898cb6e
Eltigani A <tigsatir@gmail.com>
2023-10-05 00:29:45 +0300

    step4 adding alb and deploy to zone1

---
+++ starter-code/zone1/modules/alb/alb.tf
@@ -0,0 +1,70 @@
+resource "aws_lb_target_group" "udacity" {
+  name     = "udacity-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "udacity" {
+  count            = 3
+  target_group_arn = aws_lb_target_group.udacity.arn
+  target_id        = var.ec2.*.id[count.index]
+  port             = 80
+}
+
+resource "aws_lb" "udacity" {
+  name               = "udacity-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.ec2_sg]
+  subnets            = var.subnet_id
+
+  enable_deletion_protection = false
+
+}
+
+resource "aws_lb_listener" "udacity" {
+  load_balancer_arn = aws_lb.udacity.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.udacity.arn
+  }
+}
+resource "aws_lb_target_group" "alb" {
+  name     = "a-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "alb" {
+  count            = 3
+  target_group_arn = aws_lb_target_group.alb.arn
+  target_id        = var.ec2.*.id[count.index]
+  port             = 80
+}
+
+resource "aws_lb" "alb" {
+  name               = "a-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.ec2_sg]
+  subnets            = var.subnet_id
+
+  enable_deletion_protection = false
+
+}
+
+resource "aws_lb_listener" "alb" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+}
